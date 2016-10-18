a9ca243cfd45166511286603def035b21522046a
Renzo Carbonara <gnuk0001@gmail.com>
2016-10-17 21:40:07 -0300

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ _pkgs0 = import ((import <nixpkgs> {}).fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
   rev = "4aadb9beb345898c7f7b06090b163e7f3debea5a";
-  sha256 = "0j6ppr4cr3kj8m3xd1kiah3351j1g1v5gzm0vqibj8p5h7490s6j";
-});
+  sha256 = "1dka2knhi8pfb83iwph7chldys1df95dc3z2v38cqzy4m06qjir9";
+}) {};
 
 in
 { pkgs ? _pkgs0
